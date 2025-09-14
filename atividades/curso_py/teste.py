776482311b06a8bfd81e0408b58c404f7f313a95
EDUARDO SIDNEY DA SILVA XAVIER <87551975+EduSSXavier@users.noreply.github.com>
2025-09-14 17:31:16 -0300

    carga inicial curso python

---
+++ atividades/curso_py/teste.py
@@ -0,0 +1,9 @@
+tuplaCarros = ("Fiat", "Honda", "Ford")
+
+print(tuplaCarros[0])           # Fiat
+print(tuplaCarros[1])           # Honda
+print(tuplaCarros[2])           # Ford
+print(tuplaCarros[-1])          # Ford
+print(tuplaCarros[-2])          # Honda
+
+tuplaCarros[0] = "Mercedes"     # tuplaCarros = ("Mercedes", "Honda", "Ford")
