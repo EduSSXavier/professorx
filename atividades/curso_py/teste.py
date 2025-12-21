5579067235d070955825b6c54f84938cfb61530d
EDUARDO SIDNEY DA SILVA XAVIER <87551975+EduSSXavier@users.noreply.github.com>
2025-12-21 17:25:02 -0300

    Delete atividades directory

--- atividades/curso_py/teste.py
+++
@@ -1,9 +0,0 @@
-tuplaCarros = ("Fiat", "Honda", "Ford")
-
-print(tuplaCarros[0])           # Fiat
-print(tuplaCarros[1])           # Honda
-print(tuplaCarros[2])           # Ford
-print(tuplaCarros[-1])          # Ford
-print(tuplaCarros[-2])          # Honda
-
-tuplaCarros[0] = "Mercedes"     # tuplaCarros = ("Mercedes", "Honda", "Ford")
